c3429f2b29d5ffcf845a004dbb45277362dbe098
Caeszx. <ravensvenix@gmail.com>
2025-06-11 17:07:01 +0800

    /proc/pid/maps: Fix Error.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -540,7 +540,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 bypass_orig_flow:
 #endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-		struct dentry *dentry = file->f_path.dentry;Add commentMore actions
+		struct dentry *dentry = file->f_path.dentry;
         if (dentry) {
 			const char *path = (const char *)dentry->d_name.name; 
 			if (strstr(path, "lineage")) { Add commentMore actions
